c77fe8de41c5968f163810ff1004870d2c0be2ef
wilgnerschuertz <schuertzww@icloud.com>
2022-07-20 12:18:30 -0400

    refactor: move api details from domain entity to data model

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 
+import '../models/models.dart';
 import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
@@ -17,7 +18,7 @@ class RemoteAuthentication {
           url: url,
           method: 'post',
           body: RemoteAuthenticationParams.fromDomain(params).toJson());
-      return AccountEntity.fromJson(httpResponse);
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.unauthorized
           ? DomainError.invalidCredentials
