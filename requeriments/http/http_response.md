f020534e0c10493fef6b838d3a698f1fdfc8dbd0
wilgnerschuertz <schuertzww@icloud.com>
2022-07-21 11:18:30 -0400

    feature: ensure HttpAdapter returns data if post returns 200

--- requeriments/http/http_response.md
+++ requeriments/http/http_response.md
@@ -3,8 +3,8 @@
 
 > ## SUCESSO
 1. ✅ Request com verbo http válido (post)
-2. Passar nos headers o content type JSON
-3. Chamar request com body correto
+2. ✅ Passar nos headers o content type JSON
+3. ✅ Chamar request com body correto
 4. OK - 200 e resposta com dados
 5. No content - 204 e resposta sem dados
 
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -6,12 +6,14 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class HttpAdapter {
+import 'package:tdd_clean_arch/data/http/http_client.dart';
+
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<Map> request({
     @required String url,
     @required String method,
     Map body,
@@ -21,7 +23,8 @@ class HttpAdapter {
       'accept': 'application/json',
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(url, headers: headers, body: jsonBody);
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
   }
 }
 
@@ -39,10 +42,15 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct values', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(url: url, method: 'post');
     });
 
     test('Should call post with correct Headers', () async {
+      when(client.post(any,
+              body: anyNamed('body'), headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
@@ -57,9 +65,19 @@ void main() {
     });
 
     test('Should call post without body', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       await sut.request(url: url, method: 'post');
 
       verify(client.post(url, headers: anyNamed('headers')));
+    });
+    test('Should return data if post returns 200', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, {'any_key': 'any_value'});
     });
   });
 }
