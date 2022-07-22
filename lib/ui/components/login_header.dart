9ee586233812d9cb7696d239926ffcba9f4cfc71
wilgnerschuertz <schuertzww@icloud.com>
2022-07-22 11:38:16 -0400

    feature: Add LoginPage layout

---
+++ lib/ui/components/login_header.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LoginHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      margin: EdgeInsets.only(top: 50, bottom: 32),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.topLeft,
+          colors: [
+            Theme.of(context).primaryColorLight,
+            Theme.of(context).primaryColorDark,
+          ],
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 0),
+            spreadRadius: 0,
+            blurRadius: 4,
+            color: Colors.black,
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(
+            32,
+          ),
+        ),
+      ),
+      child: Text('Image'),
+      //? Início Image
+
+      //     Image(
+      //   image: AssetImage('lib/ui/assets/logo2.png'),
+      // ),
+
+      //? Final Image
+    );
+  }
+}
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,48 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
+            LoginHeader(),
+            HeadLine1(
+              text: 'login',
             ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(
-                        Icons.email,
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          icon: Icon(
+                            Icons.email,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ),
+                        obscureText: true,
                       ),
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      icon: Icon(
-                        Icons.lock,
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Entrar'.toLowerCase(),
-                    ),
-                  ),
-                  FlatButton.icon(
+                    RaisedButton(
                       onPressed: () {},
-                      icon: Icon(Icons.person),
-                      label: Text('Criar conta'))
-                ],
+                      child: Text(
+                        'Entrar'.toUpperCase(),
+                      ),
+                    ),
+                    FlatButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.person),
+                        label: Text('Criar conta'))
+                  ],
+                ),
               ),
             )
           ],
