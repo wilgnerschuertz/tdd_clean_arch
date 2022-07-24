670b45bbb7fa417cf3e45621522bda4e57057bd0
wilgnerschuertz <schuertzww@icloud.com>
2022-07-23 21:53:33 -0400

    test: ensure LoginPage presents error if password is invalid

--- requeriments/login/login_page.md
+++ requeriments/login/login_page.md
@@ -4,12 +4,12 @@
 
 1.  [X] Os campos devem começar sem exibir mensagem de erro
 2.  [X] O Botão de fazer login deve começar desabilitado
-3.  [] Após digitar algo em um campo, a mensagem de erro só deve sumir se o campo for válido
+3.  [X] Após digitar algo em um campo, a mensagem de erro só deve sumir se o campo for válido
 4.  [X] Validar e-mail quando o usuário digitar no campo
 5.  [X] Mostrar mensagem de erro se o e-mail for inválido
-6.  [] Remover mensagem de erro se o e-mail for válido
+6.  [X] Remover mensagem de erro se o e-mail for válido
 7.  [X] Validar senha quando o usuário digitar no campo
-8.  [] Mostrar mensagem de erro se a senha for inválida
+8.  [X] Mostrar mensagem de erro se a senha for inválida
 9.  [] Remover mensagem de erro se a senha for válida
 10. [] Habilitar botão de login se todos os campos forem válidos
 11. [] Desabilitar o botão de fazer login se algum campo for inválido
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -12,18 +12,23 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 void main() {
   LoginPresenter presenter;
   StreamController<String> emailErrorController;
+  StreamController<String> passwordErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
     when(presenter.emailErrorStream)
         .thenAnswer((realInvocation) => emailErrorController.stream);
+    when(presenter.passwordErrorStream)
+        .thenAnswer((realInvocation) => passwordErrorController.stream);
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
 
   tearDown(() {
     emailErrorController.close();
+    passwordErrorController.close();
   });
 
   testWidgets('Should load with correct initial state',
@@ -102,5 +107,15 @@ void main() {
         find.descendant(
             of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
         findsOneWidget);
+  });
+
+  testWidgets('Should present error if password is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
   });
 }
